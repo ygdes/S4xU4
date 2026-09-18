f483695b5190a21f269c61fdb0490bdb84086aa6
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-19 00:25:18 +0200

    Update FullAdder_sg13.v: negated sum

--- src/FullAdder_sg13.v
+++ src/FullAdder_sg13.v
@@ -1,5 +1,4 @@
 // Just a dumb Full Adder
-
 module FullAdderSG13 (
   input  wire d1,
   input  wire d2,
@@ -12,6 +11,26 @@ module FullAdderSG13 (
   // sum:
   sg13_xor2_1 x1 (.X(t), .A(d1), .B(d2));
   sg13_xor2_1 x2 (.X(S), .A(t ), .B(d3));
+
+  // carry out:
+  sg13_a22oi_1 a(.A1(d1), .A2(d2), .B1(t), .B2(d3), .Y(u));
+  sg13_inv_1 i(.A(u), .Y(C));
+endmodule
+
+// Same but with a negated sum output
+// (same speed and area)
+module FullAdderSG13negS (
+  input  wire d1,
+  input  wire d2,
+  input  wire d3,
+  output wire Sn,
+  output wire C
+);
+  wire t, u;
+
+  // sum:
+  sg13_xor2_1  x1 (.X(t),  .A(d1), .B(d2));
+  sg13_xnor2_1 x2 (.Y(Sn), .A(t ), .B(d3));
 
   // carry out:
   sg13_a22oi_1 a(.A1(d1), .A2(d2), .B1(t), .B2(d3), .Y(u));
