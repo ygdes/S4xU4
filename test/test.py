4d301afd24552eb9e52c712ca98e9db328172451
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-20 01:21:46 +0200

    Update test.py: cleanup

--- test/test.py
+++ test/test.py
@@ -47,17 +47,3 @@ async def test_project(dut):
          diag=" ***"
       assert val == (U*S)
       dut._log.info(str(U) + " * " + str(S) + " => " + str(dut.uo_out.value) + " : " + str(val)+ diag)
-
-    # Set the input values you want to test
-    # dut.ui_in.value = 20
-    # dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    # await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    # assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
