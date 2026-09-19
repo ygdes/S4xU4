1252eab1fd6e8cc3dff4050a7f65463104465958
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-19 22:27:10 +0200

    Update test.py: more clock waiting

--- test/test.py
+++ test/test.py
@@ -38,7 +38,7 @@ async def test_project(dut):
   for U in range(0, 15):
     for S in range(-8, 7):
       dut.ui_in.value = (U << 4)|(S & 15);
-      await ClockCycles(dut.clk, 1)
+      await ClockCycles(dut.clk, 6)
       dut._log.info(str(U) + " * " + str(S) + " => " + str(dut.uo_out.value) + " : " + str(int(dut.uo_out.value)))
 
     # Set the input values you want to test
